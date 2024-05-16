e61996c69e6e8e31e005804fe66a331014da2636
Francis Bayotlang <fnbayotlang@student.apc.edu.ph>
2024-05-16 20:05:00 +0800

    PHP Simple Form Example

---
+++ week5/index.php
@@ -0,0 +1,42 @@
+<html>
+
+  <style>
+    h4     {
+      text-align: center;
+      padding: 10px; 
+      margin: auto; 
+      background-color: FF9800; 
+      }
+    h2 {
+      text-align: center; 
+    }
+    ul {
+      list-style-type: none;
+      margin: 100;
+      padding: 100;
+    }
+
+    li {
+      display: inline;
+      
+    }
+    
+    
+    
+  </style>
+  
+<body>
+  <div id="grad1">
+  <h4 style="color:blue;">Hello Traveler! Welcome to my Personal Webpage!</h4>
+
+  <h2>My First Webpage</h2>
+  </div>
+
+  
+ <?php
+ echo "Ni hao! My first PHP script!"; 
+ ?>
+  
+  
+</body>
+</html>
